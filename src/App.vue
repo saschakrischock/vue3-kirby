1611e4088bbbba9a96a79cf6a7c6a2a6613600ed
saschakrischock <s.krischock@gmail.com>
2023-01-02 20:52:34 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <SkipToContentLink />
+  <Announcer />
+
+  <div class="page">
+    <Header />
+    <main id="main" tabindex="-1">
+      <RouterView :key="route.path" />
+    </main>
+  </div>
+
+  <Footer />
+</template>
+
+<script setup>
+import { useRoute } from "vue-router";
+const route = useRoute();
+</script>
